0edf4ed3458c474f1fa4f20482ec36cb27b70371
Kishore Newton <kishorenke@gmail.com>
2020-08-25 05:08:34 +0530

    added wait until find element option

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 import os 
 import time
 
@@ -12,9 +15,13 @@ class InstagramBot:
 
     def login(self):
         self.driver.get('https://www.instagram.com/accounts/login/')
-        self.driver.find_elements_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys(self.username)
-        self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(self.password)
-        self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button').click()
+        self.driver.maximize_window()
+        self.wait = WebDriverWait(self.driver, 20)
+        self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input'))).send_keys(self.username)
+        # self.wait.until(EC.presence_of_element_located((By)))
+        # self.driver.find_elements_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys(self.username)
+        # self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(self.password)
+        # self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button').click()
 
 if __name__ == '__main__':
     ig_bot = InstagramBot('temp_username', 'temp_password')
