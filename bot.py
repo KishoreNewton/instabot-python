be21094466fa1af9cad08a22b8d108ef51fab8be
Kishore Newton <kishorenke@gmail.com>
2020-08-24 18:34:43 +0530

    added chrome driver

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import os 
 import time
 
@@ -7,6 +8,9 @@ class InstagramBot:
     def __init__(self, username, password):
         self.username = username
         self.password = password
+        CHROMEDRIVER_PATH = './chromedriver'
+        self.driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH)
+        self.driver.get('https://www.instagram.com/')
 
 if __name__ == '__main__':
     ig_bot = InstagramBot('temp_username', 'temp_password')
