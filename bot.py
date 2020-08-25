32cec0dd60c064b56f39cf73a3a24a92deb03040
Kishore Newton <kishorenke@gmail.com>
2020-08-25 09:49:16 +0530

    added the follow and unfloow button

--- bot.py
+++ bot.py
@@ -5,24 +5,59 @@ from selenium.webdriver.support.ui import WebDriverWait
 import os 
 import time
 
+# This class uses selenium to open browser and automate 
 class InstagramBot:
 
     def __init__(self, username, password):
+        """
+        Args:
+            username:str: Instagram's username
+            password:str: Instagram's password
+
+        Chromedriver location at root of folder
+        """
         self.username = username
         self.password = password
         self.driver = webdriver.Chrome('./chromedriver')
+        self.base_url = 'https://www.instagram.com'
         self.login()
 
     def login(self):
-        self.driver.get('https://www.instagram.com/accounts/login/')
+        self.driver.get(f'{self.base_url}/accounts/login/')
         self.driver.maximize_window()
         self.wait = WebDriverWait(self.driver, 20)
         self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input'))).send_keys(self.username)
         self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input'))).send_keys(self.password)
         self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="loginForm"]/div/div[3]/button/div'))).click()
-        # self.driver.find_elements_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys(self.username)
-        # self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(self.password)
-        # self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button').click()
-        # self.driver.close()
+        time.sleep(3)
+    
+    def nav_user(self, user):
+        self.driver.get(f'{self.base_url}/{user}')
+
+    def follow_user(self, user):
+        """
+        navigate the user and follow them
+        """
+        self.nav_user(user)
+        self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/section/main/div/header/section/div[1]/div[2]/a/button'))).click()
+        time.sleep(30)
+        self.unfollow_user(user)
+
+    def unfollow_user(self, user):
+        """
+        navigate the user and unfollow them
+        """
+        self.nav_user(user)
+        self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/section/main/div/header/section/div[1]/div[2]/a/button'))).click()
+    
+
 if __name__ == '__main__':
+    """
+    Args:
+        id_bot initialize the class InstagramBot with username and password 
+        ig_bot.follow_user enter the user to follow and unfollow
+
+    """
     ig_bot = InstagramBot('temp_username', 'temp_password')
+
+    ig_bot.follow_user('tonyrobbins')
